05c877e1f7bca81ed2fd931a56bc1a64de44122c
warfka <egor737614@gmail.com>
2022-05-01 23:14:46 +1000

    second

--- Java/src/main/java/HW.java
+++ Java/src/main/java/HW.java
@@ -1,15 +1,16 @@
 public class HW {
+
     public static void main(String[] args)
     {
         double a=1;
         double b=-8;
         double c=15;
-        double D= b * b - 4 * a * c + 27;
+        double D= b * b - 4 * a * c;
 
         if (D > 0) {
             double x1, x2;
-            x1 = (b - Math.sqrt(D)) / (2 * a);
-            x2 = (b + Math.sqrt(D)) / (2 * a);
+            x1 = (-b - Math.sqrt(D)) / (2 * a);
+            x2 = (-b + Math.sqrt(D)) / (2 * a);
             System.out.println("Корни уравнения: x1 = " + x1 + ", x2 = " + x2);
         }
         else if (D == 0) {
