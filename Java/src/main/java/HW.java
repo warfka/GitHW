13c9e0c3e05a6edf724975d7a0d41cf0eabaec4f
warfka <egor737614@gmail.com>
2022-05-01 23:16:02 +1000

    first

--- Java/src/main/java/HW.java
+++ Java/src/main/java/HW.java
@@ -7,19 +7,12 @@ public class HW {
         double c=15;
         double D= b * b - 4 * a * c;
 
-        if (D > 0) {
+
             double x1, x2;
             x1 = (-b - Math.sqrt(D)) / (2 * a);
             x2 = (-b + Math.sqrt(D)) / (2 * a);
             System.out.println("Корни уравнения: x1 = " + x1 + ", x2 = " + x2);
-        }
-        else if (D == 0) {
-            double x;
-            x = -b / (2 * a);
-            System.out.println("Уравнение имеет единственный корень: x = " + x);
-        }
-        else {
-            System.out.println("Уравнение не имеет действительных корней!");
-        }
+
+
     }
 }
